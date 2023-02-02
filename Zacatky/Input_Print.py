b4fa8782b368f7b8d1931f093335b874bbf81785
Dex3099 <112333650+Dex3099@users.noreply.github.com>
2023-02-02 19:58:33 +0100

    Update Input_Print.py

--- Zacatky/Input_Print.py
+++ Zacatky/Input_Print.py
@@ -1,7 +1,7 @@
-jmeno = input
-
-input("Zadej své jméno: ")
-
-print (f"Ahoj {jmeno} jak se máš?")
-
+#Proměnná jmeno ktera bere info z funkce info
+jmeno = input("Zadej své jméno: ")
+#Input Neco jako textove pole
+#vytiskne Ahoj a to co je v promenne jmeno
+print (f"Ahoj {jmeno}")
+#Print
 print("Čus")
