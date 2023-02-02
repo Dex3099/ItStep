ae4154498234c59750653841e6a23cc417cef835
Dex3099 <112333650+Dex3099@users.noreply.github.com>
2023-02-02 20:00:58 +0100

    Update Input_Print.py

--- Zacatky/Input_Print.py
+++ Zacatky/Input_Print.py
@@ -1,7 +1,8 @@
 #Proměnná jmeno ktera bere info z funkce info
 jmeno = input("Zadej své jméno: ")
 #Input Neco jako textove pole
+vek = input("Zadej svůj věk: ")
 #vytiskne Ahoj a to co je v promenne jmeno
-print (f"Ahoj {jmeno}")
+print (f"Ahoj {jmeno} máš {vek} let")
 #Print
 print("Čus")
