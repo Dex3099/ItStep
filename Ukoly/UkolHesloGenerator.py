d186098ec03bc74e16ccaaa250044b1b764ed2bf
Dex3099 <112333650+Dex3099@users.noreply.github.com>
2023-03-02 08:04:23 +0100

    Update UkolHesloGenerator.py

--- Ukoly/UkolHesloGenerator.py
+++ Ukoly/UkolHesloGenerator.py
@@ -1,56 +1,32 @@
-# import modules
+# importuje moduly
 import string
 import random
 
 
-# store all characters in lists
+# uloží všechny charaktery do listů
 s1 = list(string.ascii_lowercase)
 s2 = list(string.ascii_uppercase)
 s3 = list(string.digits)
 s4 = list(string.punctuation)
 
 
-# Ask user about the number of characters
+# Zeptá se na délku hesla
 user_input = input("How many characters do you want in your password? ")
+characters_number = int(user_input)
 
-
-# check this input is it number? is it more than 8?
-while True:
-
-	try:
-
-		characters_number = int(user_input)
-
-		if characters_number < 8:
-
-			print("Your number should be at least 8.")
-
-			user_input = input("Please, Enter your number again: ")
-
-		else:
-
-			break
-
-	except:
-
-		print("Please, Enter numbers only.")
-
-		user_input = input("How many characters do you want in your password? ")
-
-
-# shuffle all lists
+# zrandomizuje listy charakterů
 random.shuffle(s1)
 random.shuffle(s2)
 random.shuffle(s3)
 random.shuffle(s4)
 
 
-# calculate 30% & 20% of number of characters
+# kalkuluje 30% & 20% z čísel a charakterů
 part1 = round(characters_number * (30/100))
 part2 = round(characters_number * (20/100))
 
 
-# generation of the password (60% letters and 40% digits & punctuations)
+# vygeneruje heslo 60:40 ratio pismena:cisla
 result = []
 
 for x in range(part1):
@@ -64,11 +40,10 @@ for x in range(part2):
 	result.append(s4[x])
 
 
-# shuffle result
+# zrandomizuje vásledek
 random.shuffle(result)
 
 
-# join result
+# joinuje? vysledek
 password = "".join(result)
 print("Strong Password: ", password)
-
